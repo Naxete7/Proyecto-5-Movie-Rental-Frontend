3cce3b856099c81c2bfc6373afa9cb501b0ed024
Ignacio <naxete7@hotmail.es>
2022-11-20 17:20:35 +0100

    falta llamar a la API desde el Home

--- movierental/src/containers/Home/Home.jsx
+++ movierental/src/containers/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Home.scss"
+import { bringFilms } from '../../services/apicalls'
 import { Button, Space } from 'antd';
 import anillo  from '../../assets/img/anillo.png'
 
 
+import "./Home.scss"
 
 const Home = () => {
     const navigate = useNavigate();
@@ -18,7 +19,7 @@ const Home = () => {
             <div><img src="../../assets/img/anillo.png" alt="imagen de portada" /></div> 
             
 
-            <div>Alquila todas las peliculas y series que quieras.</div> 
+            <div className="textCentralDesign">Alquila todas las peliculas y series que quieras.</div> 
             
             <div className="buttonHome">
                 {/*<button className="buttonHomeDesign" onClick={() => navigate("/login")} href="../Login/Login.jsx</div>">Login</button>*/}
