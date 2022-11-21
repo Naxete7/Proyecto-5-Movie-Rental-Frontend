0d1f90d4895b02a467b1dd3cc03b231f3d54c0e7
Ignacio <naxete7@hotmail.es>
2022-11-21 11:53:45 +0100

    peliculas traidas de la Api

--- movierental/src/containers/Home/Home.jsx
+++ movierental/src/containers/Home/Home.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { bringFilms } from '../../services/apicalls'
 import { Button, Space } from 'antd';
-import anillo  from '../../assets/img/anillo.png'
+//import anillo  from '../../assets/img/anillo.png'
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 import "./Home.scss"
@@ -10,13 +12,40 @@ import "./Home.scss"
 const Home = () => {
     const navigate = useNavigate();
     
+const [movies, setMovies] = useState([]);
+ 
+    const url = "https://image.tmdb.org/t/p/original"
+
+    useEffect(() => {
+        //This function is triggered when the component is mounted for the first time.
+
+        if (movies.length === 0) {
+            // bringmovies();
+
+            setTimeout(() => {
+                //Adding a 2 seconds delay on purpose...
+
+                bringFilms().then(
+                    (res) => {setMovies(res.data.results)
+                        console.log(res)}
+                );
+
+
+            }, 2000);
+
+        };
+
+
+    }, [movies]);
+
+
     return (
 
         
         <div className="homeDesign">
           
           
-            <div><img src="../../assets/img/anillo.png" alt="imagen de portada" /></div> 
+            {/*<div><img src="../../assets/img/anillo.png" alt="imagen de portada" /></div> */}
             
 
             <div className="textCentralDesign">Alquila todas las peliculas y series que quieras.</div> 
@@ -30,7 +59,26 @@ const Home = () => {
             </div>
 
 
+<div className="moviesShowcase">
+                {/* Here I proceed to MAP the hook which contains all the movies */}
+               
+                <div className="leftSide">
+                    {
+                        movies.map(movie => {
+                            return <div  className="movieDesign" key={movie.id}>
+                                <div>{movie.original_title}</div>
+                                <div ><img className="moviePic" src={url+movie.poster_path} /></div>
+                            </div>
+                        })
+                    }
 
+                </div>
+
+
+
+
+
+            </div>
 
 
 
