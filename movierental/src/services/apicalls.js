77df2fb2dac2ec75a73ffe6a3fa670f756c96ae7
Ignacio <naxete7@hotmail.es>
2022-11-29 13:46:10 +0100

    cambio estilos peliculas

--- movierental/src/services/apicalls.js
+++ movierental/src/services/apicalls.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-var root = 'https://localhost:3001/films/';
+var root = 'https://localhost:3000/films/';
 //var apiKey = '210d6a5dd3f16419ce349c9f1b200d6d';
 
 export const loginUser = async (body) => {
@@ -13,7 +13,7 @@ console.log(body)
 // console.log(bodyReq)
 
     return axios.post(
-        "http://localhost:3001/auth/login",
+        "http://localhost:3000/auth/login",
             body
             // {
             //     "mail": 'elbanking@punsdhi.com',
@@ -31,7 +31,7 @@ export const registerUser = async (body) => {
     console.log(body)
     
         return axios.post(
-            "http://localhost:3001/auth/register",
+            "http://localhost:3000/auth/register",
                 body
                 // {
                 //     "mail": 'elbanking@punsdhi.com',
@@ -44,7 +44,7 @@ export const bringFilms = () => {
     
         try {
     
-            return axios.get("http://localhost:3001/films");
+            return axios.get("http://localhost:3000/films");
             
     
         } catch (error) {
@@ -64,19 +64,19 @@ export const bringFilms = () => {
 }
     export const bringUserInfo = (email) => {
     
-        return axios.get("http://localhost:3001/users/id/" + email)
+        return axios.get("http://localhost:3000/users/id/" + email)
         
     };
 
     export const bringUserOrders = (email) => {
     
-        return axios.get(`http://localhost:3001/orders/${email}`)
+        return axios.get(`http://localhost:3000/orders/${email}`)
 
     };
 
     export const bringAllOrders = () => {
     
-        return axios.get(`http://localhost:3001/orders/`)
+        return axios.get(`http://localhost:3000/orders/`)
 
     };
 
